2a2a44f093a5d0602a4956a158f99970c2fc6e3d
Tam Nguyen <ttamng0905@gmail.com>
2022-03-20 16:56:26 -0600

    updated

--- go-server/event_subscriber.go
+++ go-server/event_subscriber.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/big"
+	"strconv"
+	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -18,21 +21,46 @@ func main() {
 
 	contractAddress := common.HexToAddress("0x667a05757f2c27694ed46Af0205E2d392C5853c3")
 	query := ethereum.FilterQuery{
+		FromBlock: big.NewInt(2394201),
+		ToBlock:   big.NewInt(2394201),
 		Addresses: []common.Address{contractAddress},
 	}
 
-	logs := make(chan types.Log)
-	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	logs, err := client.FilterLogs(context.Background(), query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for {
-		select {
-		case err := <-sub.Err():
+	contractAbi, err := abi.JSON(strings.NewReader(string(store.StoreABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, vLog := range logs {
+		fmt.Println(vLog.BlockHash.Hex())
+		fmt.Println(vLog.BlockNumber)
+		fmt.Println(vLog.TxHash.Hex())
+
+		event := struct {
+			Address   [32]byte
+			Message   string
+			Timestamp uint32
+		}{}
+
+		err := contractAbi.Unpack(&event, "Message", vLog.Data)
+		if err != nil {
 			log.Fatal(err)
-		case vLog := <-logs:
-			fmt.Println(vLog) // pointer to event log
 		}
+
+		fmt.Println(string(event.Address[:]))
+		fmt.Println(string(event.Message))
+		fmt.Println(strconv.Itoa(int(event.Timestamp)))
+
+		//var topics [4]string
+		//for i := range vLog.Topics {
+		//	topics[i] = vLog.Topics[i].Hex()
+		//}
+
+		fmt.Println(topics[0])
 	}
 }
